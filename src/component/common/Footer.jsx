aacf72ea9be7ce083115fc5c776016b06522572f
sowish23 <soso2266@naver.com>
2020-12-07 17:47:52 +0900

    footer 수정

--- src/component/common/Footer.jsx
+++ src/component/common/Footer.jsx
@@ -6,11 +6,26 @@ const Footer = () => {
 	return (
         <div className="nav">
             <ul className="nav-list">
-                <li className="nav-btn">홈</li>
-                <li className="nav-btn">모의 지원</li>
-                <li className="nav-btn">합격 예측</li>
-                <li className="nav-btn">컨설팅</li>
-                <li className="nav-btn">설정</li>
+                <li className="nav-btn">
+                    <img src={'/assets/icons/home.svg'} />
+                    <div className="btn_title">홈</div>
+                </li>
+                <li className="nav-btn">
+                    <img src={'/assets/icons/copy.svg'} />
+                    <div className="btn_title">모의 지원</div>
+                </li>
+                <li className="nav-btn">
+                    <img src={'/assets/icons/darts.svg'} />
+                    <div className="btn_title">합격 예측</div>
+                </li>
+                <li className="nav-btn">
+                    <img src={'/assets/icons/debate.svg'} />
+                    <div className="btn_title">컨설팅</div>
+                </li>
+                <li className="nav-btn">
+                    <img src={'/assets/icons/user.svg'} />
+                    <div className="btn_title">설정</div>
+                </li>
             </ul>
         </div>
 	);
