d617f255bc2fe2492668714f7383b6c0371dadfc
sowish23 <soso2266@naver.com>
2020-12-07 13:52:51 +0900

    로그인 박스쉐도우 추가 및 카카오톡 로고 추가

--- src/component/main/Login.jsx
+++ src/component/main/Login.jsx
@@ -6,16 +6,12 @@ const Login = () => {
         <div>
             <p className="title">ING 입시연구소</p>
             <p className="sub-title">대입 정시 컨설팅</p>
-            <div className="kakao">
-                <img/>
+            <button className="kakao_btn">
+                <img className="kakao_icon" src={'/assets/icons/kakao-talk.svg'}/>
+                <kakaoTalk />
                 <span className="p1">카카오톡</span>
                 <span className="p2">으로 로그인</span>
-            </div>
-            {/* <div className="facebook">
-                <img/>
-                <span className="p1">구글</span>
-                <span className="p2">로 로그인</span>
-            </div> */}
+            </button>
         </div>
     );
 }
