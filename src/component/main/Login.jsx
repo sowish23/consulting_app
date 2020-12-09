f655d862ee5bdbf960a722f1e18c2f0a047bc1eb
sowish23 <soso2266@naver.com>
2020-12-09 15:58:43 +0900

    로그인 페이지 리뉴얼

--- src/component/main/Login.jsx
+++ src/component/main/Login.jsx
@@ -3,9 +3,11 @@ import "./Login.css";
 
 const Login = () => {
     return(
-        <div>
-            <p className="title">ING 입시연구소</p>
-            <p className="sub-title">대입 정시 컨설팅</p>
+        <div className="Login_page">
+            <div className="contents">
+                <div className="content_text1">InG 정시 합격 예측</div>
+                <div className="content_text2">대입 정시 컨설팅</div>
+            </div>
             <button className="kakao_btn">
                 <img className="kakao_icon" src={'/assets/icons/kakao-talk.svg'}/>
                 <kakaoTalk />
