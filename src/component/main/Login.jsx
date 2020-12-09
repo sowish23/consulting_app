17753e089478d2527a8691336e2ba1156df0087a
sowish23 <soso2266@naver.com>
2020-12-09 18:54:30 +0900

    [css] login

--- src/component/main/Login.jsx
+++ src/component/main/Login.jsx
@@ -4,15 +4,15 @@ import "./Login.css";
 const Login = ({history}) => {
     return(
         <div className="Login_page">
-            <div className="contents">
-                <div className="content_text1">InG 정시 합격 예측</div>
-                <div className="content_text2">대입 정시 컨설팅</div>
+            <div className="Login_contents">
+                <div className="Login_content_text1">InG 정시 합격 예측</div>
+                <div className="Login_content_text2">대입 정시 컨설팅</div>
             </div>
-            <button className="kakao_btn" onClick={() => history.push('/help')}>
-                <img className="kakao_icon" src={'/assets/icons/kakao-talk.svg'}/>
+            <button className="Login_kakao_btn" onClick={() => history.push('/help')}>
+                <img className="Login_kakao_icon" src={'/assets/icons/kakao-talk.svg'}/>
                 <kakaoTalk />
-                <span className="p1">카카오톡</span>
-                <span className="p2">으로 로그인</span>
+                <span className="Login_p1">카카오톡</span>
+                <span className="Login_p2">으로 로그인</span>
             </button>
         </div>
     );
