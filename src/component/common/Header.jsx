32d4c63ac23388fb963c130fdf84844306160505
sowish23 <soso2266@naver.com>
2020-12-10 11:24:17 +0900

    [css] Header

--- src/component/common/Header.jsx
+++ src/component/common/Header.jsx
@@ -7,12 +7,12 @@ import SideNav from "./SideNav"
 const Header = props => {
 	const [showNav, setShowNav] = useState(false)
 	return (
-        <div className="header">
+        <div className="Header">
             <SideNav show={showNav} setShowNav={setShowNav}/>
-            <div className="header_width">
-                <div className="bars" onClick={() => setShowNav(true)}><FontAwesomeIcon icon={faBars} /></div>
-                <div className="title_center">
-                    <div className="header_title">InG 정시 합격 예측</div>
+            <div className="Header_width">
+                <div className="Header_bars" onClick={() => setShowNav(true)}><FontAwesomeIcon icon={faBars} /></div>
+                <div className="Header_title_center">
+                    <div className="Header_title">InG 정시 합격 예측</div>
                 </div>
             </div>
         </div>
