cdf44b198cbc847c6d661bba23bedb924f6838fc
sowish23 <soso2266@naver.com>
2020-12-10 11:22:18 +0900

    [css] Consulting

--- src/component/consulting/Consulting.jsx
+++ src/component/consulting/Consulting.jsx
@@ -7,12 +7,12 @@ const Consulting = () => {
     return(
         <div>
             <Header />
-            <div className="Consult_page">
-                <div className="Consult_content">
-                    <div className="contents">
+            <div className="Consulting_page">
+                <div className="Consulting_content">
+                    <div className="Consulting_contents">
                         <img src={'/assets/logo/ing 로고.png'} />
-                        <div className="content_text1">현재 'AI 맞춤 컨설팅'은 서비스 준비중입니다.</div>
-                        <div className="content_text2">12월 23일 수능 성적 발표 후 <br/>모의 지원 서비스와 함께 시작됩니다.</div>
+                        <div className="Consulting_content_text1">현재 'AI 맞춤 컨설팅'은 서비스 준비중입니다.</div>
+                        <div className="Consulting_content_text2">12월 23일 수능 성적 발표 후 <br/>모의 지원 서비스와 함께 시작됩니다.</div>
                     </div>
                 </div>
             </div>
