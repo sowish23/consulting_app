f9fa2fd702474a5676990b0b8d7caeb948b7472a
sowish23 <soso2266@naver.com>
2020-12-10 11:12:22 +0900

    [css] UniversityResult

--- src/component/prediction/UniversityResult.jsx
+++ src/component/prediction/UniversityResult.jsx
@@ -4,24 +4,24 @@ import "./UniversityResult.css";
 const UniversityResult = () => {
     const [pass, setPass] = useState(true)
 	return (
-		<div className="simulation_page">
-			<div className="simulation_content">
-				<div className="simulation_detail">
-                    <div className="section">
-                        <div className="btn_tab">
-                            <button className="complete_btn">◀ 이전으로</button>
+		<div className="UniversityResult_page">
+			<div className="UniversityResult_content">
+				<div className="UniversityResult_detail">
+                    <div className="UniversityResult_section">
+                        <div className="UniversityResult_btn_tab">
+                            <button className="UniversityResult_complete_btn">◀ 이전으로</button>
                         </div>
-                        <div className="result">
-                            <div className="result_title">
+                        <div className="UniversityResult_result">
+                            <div className="UniversityResult_result_title">
                                 국민대학교 소프트웨어학과의 내 점수
                             </div>
-                            <div className="result_table">
+                            <div className="UniversityResult_result_table">
                                 <table>
                                     <thead>
                                         <tr>
                                             <td></td>
                                             <td>내 점수</td>
-                                            <td className="slash">/</td>
+                                            <td className="UniversityResult_slash">/</td>
                                             <td>만점</td>
                                         </tr>
                                     </thead>
@@ -29,60 +29,60 @@ const UniversityResult = () => {
                                         <tr>
                                             <td>국어</td>
                                             <td>80</td>
-                                            <td className="slash">/</td>
+                                            <td className="UniversityResult_slash">/</td>
                                             <td>100</td>
                                         </tr>
                                         <tr>
                                             <td>수학</td>
                                             <td>80</td>
-                                            <td className="slash">/</td>
+                                            <td className="UniversityResult_slash">/</td>
                                             <td>100</td>
                                         </tr>
                                         <tr>
                                             <td>탐구</td>
                                             <td>80</td>
-                                            <td className="slash">/</td>
+                                            <td className="UniversityResult_slash">/</td>
                                             <td>100</td>
                                         </tr>
                                         <tr>
                                             <td>영어</td>
                                             <td>80</td>
-                                            <td className="slash">/</td>
+                                            <td className="UniversityResult_slash">/</td>
                                             <td>100</td>
                                         </tr>
                                         <tr>
                                             <td>한국사</td>
                                             <td>80</td>
-                                            <td className="slash">/</td>
+                                            <td className="UniversityResult_slash">/</td>
                                             <td>100</td>
                                         </tr>
                                         <tr>
                                             <td>내신</td>
                                             <td>80</td>
-                                            <td className="slash">/</td>
+                                            <td className="UniversityResult_slash">/</td>
                                             <td>100</td>
                                         </tr>
-                                        <tr className="total_tr">
+                                        <tr className="UniversityResult_total_tr">
                                             <td>합계</td>
                                             <td>80</td>
-                                            <td className="slash">/</td>
+                                            <td className="UniversityResult_slash">/</td>
                                             <td>전형 총점 100</td>
                                         </tr>
                                     </tbody>
                                 </table>
                             </div>
-                            <div className="result_hr"></div>
-                            <div className="result_badge">
+                            <div className="UniversityResult_result_hr"></div>
+                            <div className="UniversityResult_result_badge">
                                 {
                                     pass ? 
-                                    <div className="pass_badge">합격 예상</div> :
-                                    <div className="nonpass_badge">불합격 예상</div>
+                                    <div className="UniversityResult_pass_badge">합격 예상</div> :
+                                    <div className="UniversityResult_nonpass_badge">불합격 예상</div>
                                 }
                             </div>
                         </div>
                     </div>
-                    <div className="section">
-                        <div className="result_number">
+                    <div className="UniversityResult_section">
+                        <div className="UniversityResult_result_number">
                             <table>
                                 <thead>
                                     <tr>
